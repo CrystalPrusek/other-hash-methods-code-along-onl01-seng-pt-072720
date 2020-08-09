a23fd509901de481319338404d6a298604d2a977
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-09 21:11:05 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,4 +1,18 @@
 
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
+puts groceries.values
+puts "//////////////////////"
+puts groceries.keys
+puts "//////////////////////"
+puts groceries.keys.flatten
+puts "//////////////////////"
+puts groceries.values.flatten.min
 
 
 
